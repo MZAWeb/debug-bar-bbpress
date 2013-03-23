dd93649083b8a91759fb96ad707dc4ce4d6d7f6c
Daniel Dvorkin <daniel@mzaweb.com>
2013-03-23 17:45:02 -0300

    Fix "general" tab

--- class-debug-bar-bbpress.php
+++ class-debug-bar-bbpress.php
@@ -19,29 +19,35 @@ class bbpPress_Debug_Bar extends Debug_Bar_Panel {
 	}
 
 	public function render() {
-		echo '<h2><span>';
-		echo  __( 'General:', 'bbp-debug-bar' );
-		echo '</span></h2>';
-
-		echo '<br/>';
-
-		echo '<p style="clear: both;">';
 
 		$forum = bbp_get_forum_id();
-		if ( ! empty( $forum ) )
-			echo sprintf( __( "You're seeing the forum with ID %d", "bbp-debug-bar" ), $forum );
-
 		$topic = bbp_get_topic_id();
-		if ( ! empty( $topic ) )
-			echo sprintf( __( "You're seeing the topic with ID %d", "bbp-debug-bar" ), $topic );
-
 		$reply = bbp_get_reply_id();
-		if ( ! empty( $reply ) )
-			echo sprintf( __( "You're seeing the reply with ID %d", "bbp-debug-bar" ), $reply );
+		$test  = (int) ( $forum . $topic . $reply );
 
-		echo '</p>';
+		if ( ! empty( $test ) ) {
+			echo '<h2><span>';
+			echo  __( 'General:', 'bbp-debug-bar' );
+			echo '</span></h2>';
 
-		echo '<br/>';
+			echo '<br/>';
+
+			echo '<p style="clear: both;">';
+
+
+			if ( ! empty( $forum ) )
+				echo sprintf( __( "Forum ID: %d", "bbp-debug-bar" ), $forum ) . '</br>';
+
+			if ( ! empty( $topic ) )
+				echo sprintf( __( "Topic ID: %d", "bbp-debug-bar" ), $topic ) . '</br>';
+
+			if ( ! empty( $reply ) )
+				echo sprintf( __( "Reply ID: %d", "bbp-debug-bar" ), $reply ) . '</br>';
+
+			echo '</p>';
+
+			echo '<br/>';
+		}
 
 		echo '<h2><span>';
 		echo  __( 'Loaded templates:', 'bbp-debug-bar' );
