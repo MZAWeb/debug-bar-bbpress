4716c6602cee0d1c43ab07c7dc23ac7939a661e1
Daniel Dvorkin <daniel@mzaweb.com>
2013-03-23 17:39:56 -0300

    Added current forum, topic or reply id

--- class-debug-bar-bbpress.php
+++ class-debug-bar-bbpress.php
@@ -3,7 +3,7 @@
 class bbpPress_Debug_Bar extends Debug_Bar_Panel {
 
 	/* Stores the names of the template parts loaded */
-	protected $_templates = array();
+	protected $templates = array();
 
 
 	/* Debug Bar API */
@@ -20,22 +20,47 @@ class bbpPress_Debug_Bar extends Debug_Bar_Panel {
 
 	public function render() {
 		echo '<h2><span>';
-		echo  __( 'Templates:', 'bbp-debug-bar' );
+		echo  __( 'General:', 'bbp-debug-bar' );
+		echo '</span></h2>';
+
+		echo '<br/>';
+
+		echo '<p style="clear: both;">';
+
+		$forum = bbp_get_forum_id();
+		if ( ! empty( $forum ) )
+			echo sprintf( __( "You're seeing the forum with ID %d", "bbp-debug-bar" ), $forum );
+
+		$topic = bbp_get_topic_id();
+		if ( ! empty( $topic ) )
+			echo sprintf( __( "You're seeing the topic with ID %d", "bbp-debug-bar" ), $topic );
+
+		$reply = bbp_get_reply_id();
+		if ( ! empty( $reply ) )
+			echo sprintf( __( "You're seeing the reply with ID %d", "bbp-debug-bar" ), $reply );
+
+		echo '</p>';
+
+		echo '<br/>';
+
+		echo '<h2><span>';
+		echo  __( 'Loaded templates:', 'bbp-debug-bar' );
 		echo '</span></h2>';
 
 		echo '<br/>';
 
 		echo '<ol style="clear: both; list-style: decimal;">';
-		foreach ( $this->_templates as $template ) {
+		foreach ( $this->templates as $template ) {
 			echo sprintf( '<li style="margin-left: 20px;"><p>%s</p></li>', esc_html( $template ) );
 		}
 		echo '</ol>';
+
 	}
 
 	/* Helper methods */
 
 	public function log_template_part( $templates, $slug, $name ) {
-		$this->_templates[] = $templates[0];
+		$this->templates[] = $templates[0];
 		return $templates;
 	}
 
